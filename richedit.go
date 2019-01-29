048ef67f1563ec38f3951c912a70a866bc386c38
tso <tso@teknik.io>
2019-01-29 09:53:35 -0500

    add a license or something

--- richedit.go
+++ richedit.go
@@ -1,3 +1,10 @@
+/*
+Copyright © 2018 tso <tso@teknik.io>
+
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See the LICENSE.txt file for more details.
+*/
 package main
 
 import (
